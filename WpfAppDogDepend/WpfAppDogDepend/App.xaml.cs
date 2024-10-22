6195407ca3de1d73b566895b0b4f9aa7f5948f35
Jeff Meyers <jmeyers@colum.edu>
2024-10-22 15:32:22 -0500

    Dog wpf depends

---
+++ WpfAppDogDepend/WpfAppDogDepend/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfAppDog.Models;
+
+namespace WpfAppDogDepend
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static ObservableCollection<ViewModelMammal> mammals;
+    }
+}
---
+++ WpfAppDogDepend/WpfAppDogDepend/ViewModels/ViewModelMammals.cs
@@ -0,0 +1,45 @@
+﻿using DogLibrary;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfAppDog.Models;
+using WpfAppDogDepend;
+
+namespace WpfAppDog.ViewModels
+{
+    public class ViewModelMammals
+    {
+
+        
+
+        public ObservableCollection<ViewModelMammal> Mammals
+        {
+            get;
+            set;
+        }
+
+        public ViewModelMammals(ObservableCollection<ViewModelMammal> mammals)
+        {
+            Mammals = mammals;
+        }
+
+        public void LoadMammals()
+        {
+            if (Mammals == null)
+            {
+                Mammals = new ObservableCollection<ViewModelMammal>()
+                {
+                    new ViewModelMammal ( new Dog{ BarkSound="woof!", Name="fido"}),
+                    new ViewModelMammal ( new Dog{ BarkSound="arf!", Name="rover"}),
+                    new ViewModelMammal ( new Dog{ BarkSound="arf!", Name="milo"}),
+                    new ViewModelMammal ( new Dog{ BarkSound="arf!", Name="spot"})
+                };
+            }
+
+            
+        }
+    }
+}
